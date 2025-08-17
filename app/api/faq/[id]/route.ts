36bbab563a534b3fb2889e70743e84388e499520
Muhammed Ijlan <ijlanijlu580@gmail.com>
2025-08-17 17:59:25 +0530

    update

--- app/api/faq/[id]/route.ts
+++ app/api/faq/[id]/route.ts
@@ -3,10 +3,14 @@ import { errorResponse, successResponse } from "@/app/lib/apiResponse";
 import Faq from "@/app/models/faq";
 import { z } from "zod";
 
-export async function GET(request: Request, { params }: { params: { id: string } }) {
+export async function GET(
+    request: Request,
+    context: { params: Promise<{ id: string }> }
+) {
     try {
         await connectToDB();
-        const faq = await Faq.findById(params.id);
+        const { id } = await context.params;
+        const faq = await Faq.findById(id);
         if (!faq) {
             return errorResponse("FAQ not found", 404);
         }
@@ -17,30 +21,33 @@ export async function GET(request: Request, { params }: { params: { id: string }
     }
 }
 
-export async function PUT(request: Request, { params }: { params: { id: string } }) {
+export async function PUT(
+    request: Request,
+    context: { params: Promise<{ id: string }> }
+) {
     try {
         await connectToDB();
+        const { id } = await context.params;
         const data = await request.json();
+
         const faqSchema = z.object({
             title: z.string().min(3, "Title must be at least 3 characters long"),
             content: z.string().min(5, "Content must be at least 5 characters long"),
         });
+
         const parseResult = faqSchema.safeParse(data);
         if (!parseResult.success) {
             return errorResponse(
                 parseResult.error.issues.map((e) => e.message).join(", ")
             );
         }
-        const { title, content } = parseResult.data;
 
-        const faq = await Faq.findByIdAndUpdate(
-            params.id,
-            { title, content },
-            { new: true }
-        );
+        const { title, content } = parseResult.data;
+        const faq = await Faq.findByIdAndUpdate(id, { title, content }, { new: true });
         if (!faq) {
             return errorResponse("FAQ not found", 404);
         }
+
         return successResponse(faq, "FAQ updated successfully");
     } catch (error) {
         console.error("Error updating FAQ:", error);
@@ -48,10 +55,14 @@ export async function PUT(request: Request, { params }: { params: { id: string }
     }
 }
 
-export async function DELETE(request: Request, { params }: { params: { id: string } }) {
+export async function DELETE(
+    request: Request,
+    context: { params: Promise<{ id: string }> }
+) {
     try {
         await connectToDB();
-        const faq = await Faq.findByIdAndDelete(params.id);
+        const { id } = await context.params;
+        const faq = await Faq.findByIdAndDelete(id);
         if (!faq) {
             return errorResponse("FAQ not found", 404);
         }
