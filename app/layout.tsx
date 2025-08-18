22770e06764c28110e02116bca1413bf906a4a26
Muhammed Ijlan <ijlanijlu580@gmail.com>
2025-08-18 13:48:00 +0530

    updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,11 +7,15 @@ import ThemeClientWrapper from "../theme/ThemeClientWrapper";
 export const metadata = {
   title: "Reppoo App",
   description: "This is the description of my website.",
+  icons: {
+    icon: "/favicon.svg",
+  },
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html suppressHydrationWarning lang="en" className={manrope.className}>
+      <title>Reppoo App</title>
       <body suppressHydrationWarning>
         <ThemeClientWrapper>{children}</ThemeClientWrapper>
       </body>
